2bf394101f30a28e4f32f9441b2011c25a0651a4
Aritra37 <muhuriaritra37@gmail.com>
2023-06-05 14:03:18 +0530

    connected to database and included routes

--- Server/server.js
+++ Server/server.js
@@ -10,13 +10,15 @@ app.use(cors()); //Middlewares
 app.use(cookieParser()); //Middlewares
 app.use(express.json()); //middlewares
 
-const con=mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
+const connection=mysql.createConnection({
+   host:"localhost",
+   user:"root",
+   port:3306,
+   password:"",
+   database:"Employee"
 })
 
-con.connect(function(err){
+connection.connect(function(err){
     if(err)
     {
         console.log("Error in connection");
@@ -24,6 +26,22 @@ con.connect(function(err){
     else{
         console.log("Connected");
     }
+})
+
+app.post('/login',(req,res)=>{
+    const sql="SELECT * FROM users WHERE username =? AND password = ?";
+    connection.query(sql,[req.body.username,req.body.password],(err,result)=>{
+        if(err)
+        return res.json({Error:"Error",Error:"Error running in query"});
+        if(result.length>0)
+        {
+            return res.json({Status:"Success"})
+        }
+        else
+        {
+            return res.json({Status:"Error",Error:"Wrong username or password"})
+        }
+    })
 })
 
 app.listen(8000,()=>{

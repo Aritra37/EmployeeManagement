3e72e0c93740f8b493b79fbfc1eae525996d48e5
Aritra37 <muhuriaritra37@gmail.com>
2023-06-06 16:14:47 +0530

    employee section

--- client/src/Components/Employee.jsx
+++ client/src/Components/Employee.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 function Employee() {
   return (
-    <div>Employee</div>
-  )
+    <div className="px-5 py-3">
+      <div className="d-flex justify-content-center">
+        <h3>Employee List</h3>
+      </div>
+      <Link to="/create" className="btn btn-success">Add Employee</Link>
+    </div>
+  );
 }
 
-export default Employee
+export default Employee;

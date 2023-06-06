8db3d959c1c8431901a648eae74264a9a859a3b7
Aritra37 <muhuriaritra37@gmail.com>
2023-06-06 16:15:51 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard";
 import Employee from "./Components/Employee";
 import Profile from "./Components/Profile";
-import  Home  from "./Components/Home";
+import Home  from "./Components/Home";
+import AddEmployee from "./Components/AddEmployee";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />}></Route>
-        <Route path="" element={<Home/>}></Route>
-        <Route path="/employee" element={<Employee/>}></Route>
-        <Route path="/profile" element={<Profile/>}></Route>
+        <Route path="/" element={<Dashboard />}>
+          <Route path="" element={<Home />}></Route>
+          <Route path="/employee" element={<Employee />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/create" element={<AddEmployee />}></Route>
+        </Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>

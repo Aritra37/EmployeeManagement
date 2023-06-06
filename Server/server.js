ec922570d4812ce948b42ddeffd18a72739a2359
Aritra37 <muhuriaritra37@gmail.com>
2023-06-06 16:17:00 +0530

    added form post

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import multer from 'multer';
+import path from 'path';
 
 const app=express();
 app.use(cors()); //Middlewares
@@ -16,6 +18,16 @@ const connection=mysql.createConnection({
    port:3306,
    password:"",
    database:"Employee"
+})
+
+const storage=multer.diskStorage({
+    filename:(req,file,cb)=>{
+        cb(null,file.filename+"_"+Date.now()+path.extname(file.originalname));
+    }
+})
+
+const upload=multer({
+    storage:storage
 })
 
 connection.connect(function(err){
@@ -41,6 +53,27 @@ app.post('/login',(req,res)=>{
         {
             return res.json({Status:"Error",Error:"Wrong username or password"})
         }
+    })
+})
+
+app.post('/create',(req,res)=>{
+    const sql="INSERT INTO EmployeeList (`name`,`email`,`contact`,`department`,`joiningDate`,`password`) VALUES(?)";
+    bcrypt.hash(req.body.password?.toString(),10,(err,hash)=>{
+        if(err) 
+        return res.json({Error:"Error in hashing password"});
+        const values=[
+            req.body.name,
+            req.body.email,
+            req.body.contact,
+            req.body.department,
+            req.body.joiningDate,
+            hash,
+        ]
+        connection.query(sql,[values],(err,result)=>{
+            if(err)
+            return res.json({Error:"Inside signup query"});
+            return res.json({Status:"Success"});
+        })
     })
 })
 

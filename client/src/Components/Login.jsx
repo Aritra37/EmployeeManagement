8c13347678ff6b2ecaedd800dd767502cc623032
Aritra37 <muhuriaritra37@gmail.com>
2023-06-06 16:15:21 +0530

    updated

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../styles/index.css";
+import "../styles/login.module.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 function Login() {

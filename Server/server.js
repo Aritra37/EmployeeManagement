efff7edca8a46db5070732e189a28d7472fd6615
Aritra37 <muhuriaritra37@gmail.com>
2023-06-09 13:21:25 +0530

    added employee login route

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import mysql from 'mysql';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -55,7 +55,7 @@ app.post('/login',(req,res)=>{
         if(result.length>0)
         {
             const id=result[0].id;
-            const token=jwt.sign({id},"jwt-secret-key",{expiresIn:'1d'});
+            const token=jwt.sign({role:"admin"},"jwt-secret-key",{expiresIn:'1d'});
             res.cookie('token',token);
             return res.json({Status:"Success"})
         }
@@ -65,6 +65,35 @@ app.post('/login',(req,res)=>{
         }
     })
 })
+app.post("/employeelogin", (req, res) => {
+  const sql = "SELECT * FROM employees WHERE email";
+  connection.query(sql,[req.body.email],(err, result) => {
+      if (err)
+        return res.json({ Error: "Error", Error: "Error in running query" });
+      if (result.length > 0) {
+        bcrypt.compare(req.body.password?.toString(),result[0].password,(err,response)=>{
+            if(err) return res.json({ Error:"password error"});
+            if(response)
+            {
+                const token=jwt.sign({role:"employee",id:result[0].id},"jwt-secret-key",{expiresIn:'1d'});
+                res.cookie('token',token);
+                return res.json({Status:"Success",id:result[0].id})
+            }
+            else
+            {
+                console.log(res)
+                return res.json({Status:"Error",Error:"Invalid username or password"});
+            }
+        })
+      } 
+      else {
+        return res.json({
+          Status: "Error",
+          Error: "Wrong username or password",
+        });
+      }
+    });
+});
 
 app.post('/create',(req,res)=>{
     const sql="INSERT INTO employees (`name`,`email`,`contact`,`department`,`joiningDate`,`password`) VALUES(?)";
@@ -111,13 +140,24 @@ const UserVerfication=(req,res,next)=>{
     else
     {
         jwt.verify(token,"jwt-secret-key",(err,decoded)=>{
-            if(err)return res.json({Error:"Wrong Token"})
+            if(err)return res.json({Error:"Wrong Token"});
+            req.role=decoded.role;
+            req.id=decoded.id;
             next();
         })
     }
 }
+app.get('/get/:id',(req,res)=>{
+    const id=req.params.id;
+    const sql="SELECT * FROM employees where id=?";
+    connection.query(sql,[id],(err,result)=>{
+        if(err)res.json({Error:"Cannot get employee in sql"});
+        return res.json({Status:"Success",Result:result})
+    })
+})
+
 app.get('/dashboard',UserVerfication,(req,res)=>{
-    return res.json({Status:"Success"});
+    return res.json({Status:"Success",role:req.role,id:req.id});
 })
 
 app.get('/logout',(req,res)=>{

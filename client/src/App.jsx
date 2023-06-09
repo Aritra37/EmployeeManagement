5910b86670a3bebee057bec88e13feaa82a5c73f
Aritra37 <muhuriaritra37@gmail.com>
2023-06-09 12:52:50 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import Login from "./Components/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard";
 import Employee from "./Components/Employee";
-import Profile from "./Components/Profile";
 import Home  from "./Components/Home";
 import AddEmployee from "./Components/AddEmployee";
 import Start from "./Components/Start";
@@ -17,7 +16,6 @@ function App() {
         <Route path="/" element={<Dashboard />}>
           <Route path="" element={<Home />}></Route>
           <Route path="/employee" element={<Employee />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
           <Route path="/create" element={<AddEmployee />}></Route>
         </Route>
         <Route path="/login" element={<Login />}></Route>

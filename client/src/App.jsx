c3b842d168d856a0381c4e456660c69a52523630
Aritra37 <muhuriaritra37@gmail.com>
2023-06-13 23:43:05 +0530

    updated app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import AddEmployee from "./Components/AddEmployee";
 import Start from "./Components/Start";
 import EmployeeDetails from "./Components/EmployeeDetails";
 import EmployeeLogin from "./Components/EmployeeLogin";
+import EmployeeDashboard from "./Components/EmployeeDashboard";
+import AddTask from "./Components/AddTask";
+import Home1 from "./Components/Home1";
 
 function App() {
   return (
@@ -18,10 +21,17 @@ function App() {
           <Route path="/employee" element={<Employee />}></Route>
           <Route path="/create" element={<AddEmployee />}></Route>
         </Route>
+        <Route path="/dash" element={<EmployeeDashboard />}>
+          <Route path="/dash/home" element={<Home1 />}></Route>
+          <Route path="/dash/addtask" element={<AddTask />}></Route>
+          <Route
+            path="/dash/employeedetails/:id"
+            element={<EmployeeDetails />}
+          ></Route>
+        </Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/start" element={<Start />}></Route>
         <Route path="/employeelogin" element={<EmployeeLogin />}></Route>
-        <Route path="/employeedetails/:id" element={<EmployeeDetails />}></Route>
       </Routes>
     </BrowserRouter>
   );

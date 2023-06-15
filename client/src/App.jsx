a87f793c88a6707ea554928364e88de1f551d3f2
Aritra37 <muhuriaritra37@gmail.com>
2023-06-15 13:34:07 +0530

    app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,17 @@
 import React from "react";
-import Login from "./Components/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
-import Employee from "./Components/Employee";
-import Home  from "./Components/Home";
-import AddEmployee from "./Components/AddEmployee";
-import Start from "./Components/Start";
-import EmployeeDetails from "./Components/EmployeeDetails";
-import EmployeeLogin from "./Components/EmployeeLogin";
-import EmployeeDashboard from "./Components/EmployeeDashboard";
-import AddTask from "./Components/AddTask";
-import Home1 from "./Components/Home1";
+import { Provider } from "react-redux";
+import Layout from "./Components/layout";
+
+import "./index.scss";
+import store from "./store.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />}>
-          <Route path="" element={<Home />}></Route>
-          <Route path="/employee" element={<Employee />}></Route>
-          <Route path="/create" element={<AddEmployee />}></Route>
-        </Route>
-        <Route path="/dash" element={<EmployeeDashboard />}>
-          <Route path="/dash/home" element={<Home1 />}></Route>
-          <Route path="/dash/addtask" element={<AddTask />}></Route>
-          <Route
-            path="/dash/employeedetails/:id"
-            element={<EmployeeDetails />}
-          ></Route>
-        </Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/start" element={<Start />}></Route>
-        <Route path="/employeelogin" element={<EmployeeLogin />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <Provider store={store}>
+        <Layout />
+      </Provider>
+    </div>
   );
 }
 

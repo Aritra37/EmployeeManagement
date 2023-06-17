a1bf74ac6cc3de57b64e595e33065003bda20d05
Aritra37 <muhuriaritra37@gmail.com>
2023-06-17 09:29:12 +0530

    index file for navbar

--- client/src/Components/navbar/index.jsx
+++ client/src/Components/navbar/index.jsx
@@ -1,25 +1,22 @@
 import React from "react";
-import blackLogo from "./Logo_Dark_Theme.svg";
-import lightLogo from "./Logo_Light_Theme.svg";
 import "./index.scss";
 
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 import { LogOutAction } from "../actions.js";
 function Navbar() {
   var isLoggedIn = useSelector((state) => state.isLoggedIn);
   const dispatch = useDispatch();
-  const navi = useNavigate();
+  const navigate = useNavigate();
   function handleLogClick(event) {
     event.preventDefault();
     if (isLoggedIn) {
       // console.log(" we here ");
       dispatch(LogOutAction());
-      navi("/");
+      navigate("/");
     } else {
-      navi("/");
+      navigate("/");
     }
   }
 
@@ -27,12 +24,10 @@ function Navbar() {
     <nav className="nav">
       <div className="logos">
         <div className="singleElement">
-          <img src={lightLogo} alt="" />
-          <h2>LogBook</h2>
+          <h2>Tracker</h2>
         </div>
       </div>
       <div className="display-mode">
-        {/* <MdDarkMode className="mode-button" size={35.75}></MdDarkMode> */}
         <button onClick={handleLogClick} className="LogButton button">
           {isLoggedIn ? "LogOut" : "LogIn"}
         </button>

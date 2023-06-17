5a1356692502c3b3e5608f024e0afa170c24b00f
Aritra37 <muhuriaritra37@gmail.com>
2023-06-17 09:21:58 +0530

    create user

---
+++ sourceFiles/createUser.js
@@ -0,0 +1,22 @@
+import Employees from "../models/users.js";
+
+function createUser() {
+  const d = new Date();
+  var Employee = new Employees({
+    name: "Admin",
+    email: "admin@gmail.com",
+    password: "admin",
+    contact: "1234",
+    joining: d.getTime(),
+    department: "Dep",
+    admin: true,
+  });
+  try {
+    Employee.save();
+    console.log("employee saved succesfully");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export default createUser;

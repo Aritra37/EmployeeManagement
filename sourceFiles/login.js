b438fcf433522a6eae592ed85ce0b91c2238bb68
Aritra37 <muhuriaritra37@gmail.com>
2023-06-24 16:52:12 +0530

    fixed bugs

--- sourceFiles/login.js
+++ sourceFiles/login.js
@@ -2,28 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import Employees from "../models/users.js";
 
-const app = express();
+const app = express(); //routes
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post("/", (req, res) => {
+app.post("/signin", (req, res) => {
   const data = req.body;
 
   console.log("we in backend on login");
-  console.log(data);
-  Employees.findOne({ email: data.email }, function (err, result) {
-    if (err) {
-      console.log("we got a error");
-      console.log(err);
-    } else {
-      if (result == null) {
-        res.send("NO USER");
-      } else if (data.password != result.password) {
-        res.send("INVALID LOGIN");
-      } else res.send(result);
-    }
-  });
+  Employees.findOne({ email: data.email })
+  .then((result)=>{
+      console.log(result);
+      if(result==null)
+      res.json({status : 400 });
+      else
+      res.json({status : 200, result : result });
+  })
+  .catch((err)=>{
+    console.log(err)
+    res.json({status : 401});
+  })
 });
 
 export default app;

ecf1060d1928dafd21198a680f33f8490ed4f61c
Aritra37 <muhuriaritra37@gmail.com>
2023-06-27 15:17:19 +0530

    add task issue fixed

--- sourceFiles/addTask.js
+++ sourceFiles/addTask.js
@@ -2,14 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import Tasks from "../models/tasks.js";
 
-var app = express();
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
-  var data = req.body;
-  console.log(data);
-  var task = new Tasks({
+  const data = req.body;
+  const task = new Tasks({
     email: data.email,
     type: data.type,
     start: data.startTime,
@@ -19,7 +18,6 @@ app.post("/", (req, res) => {
 
   try {
     task.save();
-    console.log("task saved");
     res.send("task saved succesfully");
   } catch (err) {
     console.log("therre was an error trying to save task ");

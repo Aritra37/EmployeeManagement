35f158510b137fc0b88a1c63ac6d0e09edc18ec1
Aritra37 <muhuriaritra37@gmail.com>
2023-06-30 23:51:08 +0530

    fixed issue on delete

--- client/src/Components/admin/deleteEmployee.jsx
+++ client/src/Components/admin/deleteEmployee.jsx
@@ -1,27 +1,35 @@
-import React from "react";
+import {React,useState,useEffect} from "react";
 import "./deleteEmployee.scss";
 import axios from "axios";
 
 function DeleteButton(props) {
-  const data = props.props;
-  let resp=[];
-  const deleteClicked = async (e) => {
-    e.preventDefault();
-    const toSend = { email: data.idSelected };
-    const response = await axios
-    .post("http://localhost:8000/deleteEmployee",toSend)
-    .then((res)=>{
-        console.log(res);
-        if (resp.status==200) 
-        {
-          alert("Employee Deleted Succesfully");
-          data.setDeleteShow(false);
-        }
-    })
+  const [count, setCount] = useState(0);
+  const data = props.props
+  const [resp ,setresp] = useState([])
+  const [show , setshow] = useState(false)
+  useEffect(()=>{
+    const deleteClicked = async () => {
+      const toSend = { email: data.idSelected };
+      const response = await axios
+      .post("http://localhost:8000/deleteEmployee",toSend)
+      .then((res)=>{
+          console.log(res);
+          setresp(res)
+          if (resp.status==200) 
+          {
+            alert("Employee Deleted Succesfully");
+          }
+      })
+    };
+    deleteClicked();
+  },[count])
+
+  const modalStyles = {
+    display: show ? 'none' : 'block'
   };
 
   return (
-    <div className="modal">
+    <div className="modal" style={modalStyles} >
       <div className="container">
         <div className="data">
           <h3>Are you absolutely sure?</h3>
@@ -33,10 +41,9 @@ function DeleteButton(props) {
         </div>
         <div className="buttons">
           <button
-            onClick={(e) => {
-              e.preventDefault();
-              data.setDeleteShow(false);
-            }}
+          onClick={()=>{
+            setshow(true);
+          }}
             id="btn1"
             className="button"
             type="button"
@@ -44,7 +51,10 @@ function DeleteButton(props) {
             No, Dont Delete
           </button>
           <button
-            onClick={deleteClicked}
+            onClick={()=>{setCount(count=>count+1)
+            setshow(true)
+            }
+          }
             id="btn2"
             className="button"
             type="button"
